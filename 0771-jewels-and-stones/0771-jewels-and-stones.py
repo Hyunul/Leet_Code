1815ca5815cee05e076628ef4592e244b1bca7a3
Hyunul <101079472+Hyunul@users.noreply.github.com>
2024-08-01 10:00:25 +0900

    Time: 4 ms (99.47%), Space: 11.4 MB (95.08%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,17 +1,7 @@
 class Solution(object):
     def numJewelsInStones(self, jewels, stones):
-        """
-        :type jewels: str
-        :type stones: str
-        :rtype: int
-        """
-        jewels_list = list(jewels)
-        stones_list = list(stones)
-        jewel_cnt = 0
-
-        for i in range(len(stones_list)):
-            for j in range(len(jewels_list)):
-                if jewels_list[j] == stones_list[i]:
-                    jewel_cnt += 1
-        
-        return jewel_cnt
+        cnt = 0
+        for i in range(len(stones)):
+            if stones[i] in jewels:
+                cnt += 1
+        return cnt
