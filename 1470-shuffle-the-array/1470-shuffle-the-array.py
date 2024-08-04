c1d3042934154c621966539bb755103261dc0372
Hyunul <101079472+Hyunul@users.noreply.github.com>
2024-08-05 07:50:16 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        x = []
+        y = []
+        result = []
+        for i in range(len(nums)):
+            if i < n:
+                x.append(nums[i])
+            else:
+                y.append(nums[i])
+
+        for j in range(len(nums)):
+            result.append(x[j])
+            result.append(y[j])
+        
+        return result
