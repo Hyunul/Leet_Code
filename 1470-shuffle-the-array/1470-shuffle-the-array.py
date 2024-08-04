16542a2160f2d14940cd346b983cf8cf82e74d15
Hyunul <101079472+Hyunul@users.noreply.github.com>
2024-08-05 07:56:04 +0900

    Time: 42 ms (18.61%), Space: 11.8 MB (69.3%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,10 +1,8 @@
 class Solution(object):
     def shuffle(self, nums, n):
-        x = []
-        y = []
         result = []
         for i in range(n):
             result.append(nums[i])
-            result.append(nums[i+3])
+            result.append(nums[i+n])
 
         return result
